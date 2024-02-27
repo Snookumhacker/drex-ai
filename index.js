f5197ebaf0d8c16b9c30d695edb4311f56eace3e
Snookumhacker <161304485+Snookumhacker@users.noreply.github.com>
2024-02-27 10:05:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const sessionName = "dreaded1";
 const antiforeign = process.env.ANTIFOREIGN || 'FALSE';
 const autobio = process.env.AUTOBIO || 'TRUE';
-let botname = process.env.BOTNAME || '𝐃𝐑𝐄𝐗_𝐀𝐈';
+let botname = process.env.BOTNAME || 'Snookum_𝐀𝐈';
 
-const owner = process.env.DEV || '254102074064'; // This will send a notification once the bot reconnects
+const owner = process.env.DEV || '254712153539'; // This will send a notification once the bot reconnects
 const {
   default: dreadedConnect,
   useMultiFileAuthState,
